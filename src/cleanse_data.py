f9be971670cacf70662fa6541dcb4b296b6d5589
Stephen <s.tseng@watsonholmes.nl>
2026-01-25 14:09:43 +0100

    fix version control and create spark dataframe

---
+++ src/cleanse_data.py
@@ -0,0 +1,7 @@
+def clean_columns_names(sdf: "pyspark.sql.DataFrame") -> "pyspark.sql.DataFrame":
+    """Cleanse the column names of a Spark DataFrame by converting them to lowercase
+    and replacing spaces with underscores.
+    """
+    new_cols = [col.lower().replace(" ", "_") for col in sdf.columns]
+    sdf = sdf.toDF(*new_cols)
+    return sdf
